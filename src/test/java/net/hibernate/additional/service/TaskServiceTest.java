a11a24b2cd6ca3332ee98f9b77dacbc6631a5b08
sergichakov <sergejsmelchakov@gmail.com>
2024-09-13 15:04:12 +0400

    just to show code

--- src/test/java/net/hibernate/additional/service/TaskServiceTest.java
+++ src/test/java/net/hibernate/additional/service/TaskServiceTest.java
@@ -31,16 +31,15 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskServiceTest {
-    TaskService taskService;//=new TaskService(new SessionRepo());;/////taskService=new TaskService(new SessionRepo());
+    TaskService taskService;
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.3")
             .withUsername("anton")
             .withPassword("anton")
-            //.withReuse(true)////////////////////////////////////////////////////////////////////
-            //.withExposedPorts(5432)////////////////////////////////////////////
+            //.withReuse(true)///testcontainers.reuse.enable=true	//   ~/.testcontainers.properties
             .withDatabaseName("postgres");
     public static void createTableConsistency()  {
         try(
-                Connection connection= DriverManager.getConnection(//"jdbc:postgresql://localhost:32770/postgres","anton","anton"))
+                Connection connection= DriverManager.getConnection(
                         postgres.getJdbcUrl(),postgres.getUsername(),postgres.getPassword()))
         {
             String createTable="create table users ("
@@ -70,7 +69,7 @@ class TaskServiceTest {
 
         postgres.start();
 
-        createTableConsistency();
+        //createTableConsistency();
     }
     public static void testConsistency(String str)  {
         try(
@@ -90,22 +89,20 @@ class TaskServiceTest {
     }
     @AfterAll
     static void afterAll()  {
-
-        //}
         postgres.stop();
     }
-  //  @BeforeEach
+    @BeforeEach
     void setUp() throws LiquibaseException, SQLException {
-        //static PostgreSQLContainer<?> postgre=new PostgreSQLContainer<>();
         Connection connection= DriverManager.getConnection(//"jdbc:postgresql://localhost:5432/testdb", "test","test");
         postgres.getJdbcUrl(),postgres.getUsername(),postgres.getPassword());
         Database dataBase= DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
         Liquibase liquibase=new Liquibase("liquibase/dev/dbchangelog.xml",new ClassLoaderResourceAccessor(),dataBase);
-        ///home/user/IdeaProjects/hibernate_additional/
         liquibase.update();
-        liquibase.close();
-        //dataBase.close();
+        //liquibase.close();
+        dataBase.close();
         connection.close();
+
+
         try(Session session=new SessionRepo().getSession().openSession()){
             Transaction transaction=session.beginTransaction();
             UserEntity userEntity=UserEntity.builder()
@@ -130,17 +127,7 @@ class TaskServiceTest {
             session.persist(taskEntity);
             transaction.commit();
         }
-        /*connection= DriverManager.getConnection(//"jdbc:postgresql://localhost:5432/testdb", "test","test");
-                postgres.getJdbcUrl(),postgres.getUsername(),postgres.getPassword());
-        PreparedStatement statement = connection.prepareStatement("select * from users");
-        ResultSet resultSet=statement.executeQuery();
-        //while (resultSet.next()) {
-        resultSet.next();
-            System.out.println("preparedStatement       :"+resultSet.getString(1)+" 2"
-                    +resultSet.getString(1)+" 3"+resultSet.getString(4));
-        //}
-        connection.close();
-        */
+
         System.out.printf("liquibase rebuild");
         taskService=new TaskService(new SessionRepo());
 
@@ -152,18 +139,9 @@ class TaskServiceTest {
                 postgres.getJdbcUrl(),postgres.getUsername(),postgres.getPassword());
         Database dataBase= DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
         Liquibase liquibase=new Liquibase("liquibase/dev/dbchangelog.xml",new ClassLoaderResourceAccessor(),dataBase);
-        ///home/user/IdeaProjects/hibernate_additional/
-////////////////////////////////////////////////////////////////////////////////////////////        //liquibase.rollback("initialState","legacy");
-        //PreparedStatement statement = connection.prepareStatement("select * from users");
-        //ResultSet resultSet=statement.executeQuery();
-
-
-
-        //if (!resultSet.first())
-        //    System.out.printf("Empty table");;
-
+        connection.close();
+        dataBase.close();
         System.out.printf("liquibase rollback");
-
     }
 
     @Test
@@ -172,7 +150,7 @@ class TaskServiceTest {
                 .name("ADMIN")
                 .password("ADMIN")
                 .build();
-        taskService=new TaskService(new SessionRepo());//kill this line from here !!!
+        //taskService=new TaskService(new SessionRepo());//kill this line from here !!!
         //try
 testConsistency("before taskDTO");
         SessionRepository sessRepo= new SessionRepo();
@@ -180,45 +158,12 @@ testConsistency("before taskDTO");
 
 //            List<TaskDTO> taskDtoList = taskService.listAllTasks(sessionObject, 1, 3);
 testConsistency("After everything");
-            //System.out.println("taskDto=" + taskDtoList);
-        /*}catch(AuthenticationException e){
-            String st=e.getMessage();
-            System.out.println("messageFrom exception"+st);
-        }
 
-         */
-        //List<TaskDTO> taskDtoList = taskService.listAllTasks(sessionObject, 1, 3);
-    }
-/*
-    @Test
-    void editTask() {
     }
 
-    @Test
-    void createTask() {
-    }
-
-    @Test
-    void deleteTask() {
-    }
-
-    @Test
-    void getAllCount() {
-    }
-
-    @Test
-    void getIdOfTag() {
-    }
-
-    @Test
-    void getAuthenticatedUser() {
-    }
-    */
 
     private static class SessionRepo implements SessionRepository{
         public SessionFactory getSession(){
-            //postgres.ses
-            //Persistence.createEntityManagerFactory();
             System.setProperty("db.port", postgres.getFirstMappedPort().toString());
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
             SessionFactory sessionFactory = emf.unwrap(SessionFactory.class);
